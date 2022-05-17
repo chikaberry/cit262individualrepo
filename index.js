1642587b4ac1f0745bc4a6efd220ecbb000c421c
chikaberry <79671470+chikaberry@users.noreply.github.com>
2022-05-17 11:19:48 -0600

    yaml file

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const application = express();
+    
+application.get('/',(req,res)=>{res.send("Hello")});
+
+application.listen(3000,()=>{console.log("listening......")});
+
