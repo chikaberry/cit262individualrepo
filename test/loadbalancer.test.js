2136a0b040102c7c8ec946f188ddf18cff069f22
chikaberry <79671470+chikaberry@users.noreply.github.com>
2022-06-07 11:17:03 -0600

    changes

--- test/loadbalancer.test.js
+++ test/loadbalancer.test.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 import assert from 'assert';
 
+
 it ('should get hello', async ()=>{
     const response = await fetch('http://35.194.74.149');
     const text = await response.text();
